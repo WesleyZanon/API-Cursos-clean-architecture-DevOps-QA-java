9c709a9bbbbe5fa5e1696f0eeae56b76ce87af65
WesleyZanon <wesleyzanon17@gmail.com>
2023-11-25 20:09:23 -0300

    updat

--- proctipe.sh
+++
@@ -1,6 +0,0 @@
-web:  java $JAVA_OPTS -Dserver.port=$PORT -Dspring.profiles.active=prod -jar target/*.war
-
-spring.datasource.url-jdbc:postgresql:localhost:5432/blog-cursoscomp
-spring.datasource.username=postgres
-spring.datasource.password=adl123
-spring.jpa.hibernate.ddl-auto=updat
--- src/main/java/com/projects/praticandoAPI/PraticandoApiApplication.java
+++ src/main/java/com/projects/praticandoAPI/PraticandoApiApplication.java
@@ -9,19 +9,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @Controller
 @SpringBootApplication
 @EnableSwagger2
-
 public class PraticandoApiApplication  {
 
 	public static void main(String[] args) {
@@ -33,12 +23,8 @@ public class PraticandoApiApplication  {
 	@ResponseBody
 	String home()
 	{
-		return  "index.html";
-
+		return "Hello World ";
 	}
 	
 	
 }
-
-
-
